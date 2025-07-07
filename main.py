11f2b5051abcc54124352d35a0b93c2a24ffa51d
JumpstartAi <jumpstartai30@gmail.com>
2025-07-08 00:31:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from chatgpt_plugin_fastapi_langchain_chroma import create_app
 
-from fastapi import FastAPI
-app = FastAPI()
+app = create_app()
 
 from routers.ask import router as ask_router
 app.include_router(ask_router)
